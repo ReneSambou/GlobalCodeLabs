26d81ed7201b5353fb7a9183673e3ae2a3371d8d
ReneSambou <sambourene9@gmail.com>
2023-08-25 16:36:10 +0100

    created a distance calcuolator

---
+++ geocoding_lab.py
@@ -0,0 +1,39 @@
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+
+geolocator = Nominatim(user_agent ='Distance calculator')
+i = 1
+while i == 1 :
+    cityA = (input("input a city: ")).capitalize()
+    cityB = (input("input another city: ")).capitalize()
+
+
+
+    try :
+    
+        locationA = geolocator.geocode(cityA)
+        locationB = geolocator.geocode(cityB)
+
+        A = (locationA.latitude, locationA.longitude)
+        B = (locationB.latitude, locationB.longitude)
+
+        Distance = geodesic(A, B)
+    
+
+
+
+        print(Distance)
+        print(f'The distance between {cityA} and {cityB} is {Distance} ')
+    
+    except:
+        print("Invalid input try again")
+    
+
+
+    
+
+
+    
+    
+
+
